60e7596be343f23c62b6ea9c0b6bb33c8a55bf9e
VeenLee <415049053@qq.com>
2019-12-25 18:06:21 +0800

    Create TestOpenGL.cpp

---
+++ 06_MoreAttrib/TestOpenGL.cpp
@@ -0,0 +1,158 @@
+//
+// 把颜色数据加进顶点数据中，并使用封装的shader class
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "shader_s.h"
+
+//回调函数，会在每次窗口大小被调整的时候被调用
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	float vertices[] = {
+		// 位置              // 颜色
+		0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+		0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f    // 顶部
+	};
+
+	/*
+	顶点数组对象(Vertex Array Object, VAO)可以像顶点缓冲对象那样被绑定，任何随后的顶点属性调用都会储存在这个VAO中。这样的好处就是，当配置顶点属性指针时，你只需要将那些调用执行一次，
+	之后再绘制物体的时候只需要绑定相应的VAO就行了。这使在不同顶点数据和属性配置之间切换变得非常简单，只需要绑定不同的VAO就行了。刚刚设置的所有状态都将存储在VAO中。
+	OpenGL的核心模式要求我们使用VAO，所以它知道该如何处理我们的顶点输入。如果我们绑定VAO失败，OpenGL会拒绝绘制任何东西。
+	*/
+	//创建一个VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	//生成一个VBO对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	//把新创建的缓冲绑定到GL_ARRAY_BUFFER目标上
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//复制顶点数组到缓冲中供OpenGL使用
+	/*
+	glBufferData是一个专门用来把用户定义的数据复制到当前绑定缓冲的函数。
+	它的第一个参数是目标缓冲的类型：顶点缓冲对象当前绑定到GL_ARRAY_BUFFER目标上。
+	第二个参数指定传输数据的大小(以字节为单位)；用一个简单的sizeof计算出顶点数据大小就行。
+	第三个参数是我们希望发送的实际数据。
+	第四个参数指定了我们希望显卡如何管理给定的数据。它有三种形式：
+		GL_STATIC_DRAW ：数据不会或几乎不会改变。
+		GL_DYNAMIC_DRAW：数据会被改变很多。
+		GL_STREAM_DRAW ：数据每次绘制时都会改变。
+	三角形的位置数据不会改变，每次渲染调用时都保持原样，所以它的使用类型最好是GL_STATIC_DRAW。
+	如果，比如说一个缓冲中的数据将频繁被改变，那么使用的类型就是GL_DYNAMIC_DRAW或GL_STREAM_DRAW，这样就能确保显卡把数据放在能够高速写入的内存部分。
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	Shader ourShader("shader.vs", "shader.fs");
+
+	//OpenGL还不知道它该如何解释内存中的顶点数据，以及它该如何将顶点数据链接到顶点着色器的属性上。
+	//我们必须在渲染前指定OpenGL该如何解释顶点数据。
+	/*
+	我们的顶点缓冲数据会被解析为下面这样子：
+		1.位置数据被储存为32位（4字节）浮点值。
+		2.每个位置包含3个这样的值。
+		3.在这3个值之间没有空隙（或其他值）。这几个值在数组中紧密排列(Tightly Packed)。
+		4.数据中第一个值在缓冲开始的位置。
+	*/
+
+	/*
+	glVertexAttribPointer函数的参数非常多，所以我会逐一介绍它们：
+	(1)index
+	指定要修改的顶点属性的索引值
+	(2)size
+	指定每个顶点属性的组件数量。必须为1、2、3或者4。初始值为4。（如position是由3个（x,y,z）组成，而颜色是4个（r,g,b,a））
+	(3)type
+	指定数组中每个组件的数据类型。可用的符号常量有GL_BYTE, GL_UNSIGNED_BYTE, GL_SHORT,GL_UNSIGNED_SHORT, GL_FIXED, 和 GL_FLOAT，初始值为GL_FLOAT。
+	(4)normalized
+	指定当被访问时，固定点数据值是否应该被归一化（GL_TRUE）或者直接转换为固定点值（GL_FALSE）。
+	(5)stride
+	指定连续顶点属性之间的偏移量。如果为0，那么顶点属性会被理解为：它们是紧密排列在一起的。初始值为0。
+	(6)pointer
+	指定第一个组件在数组的第一个顶点属性中的偏移量。该数组与GL_ARRAY_BUFFER绑定，储存于缓冲区中。初始值为0；
+	*/
+	//设置顶点位置属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	//启用顶点属性，顶点属性默认是禁用的
+	glEnableVertexAttribArray(0);
+
+	// 颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// 输入
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ourShader.use();
+		// draw our first triangle
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+		//要想绘制我们想要的物体，OpenGL给我们提供了glDrawArrays函数，它使用当前激活的着色器，之前定义的顶点属性配置，和VBO的顶点数据（通过VAO间接绑定）来绘制图元。
+		//第一个参数是我们打算绘制的OpenGL图元的类型。第二个参数指定了顶点数组的起始索引，我们这里填0。最后一个参数指定我们打算绘制多少个顶点，这里是3（我们只从我们的数据中渲染一个三角形，它只有3个顶点长）。
+		glDrawArrays(GL_TRIANGLES, 0, 3); //若想绘制两个三角形，第三个参数可以改为6
+		// glBindVertexArray(0); // no need to unbind it every time 
+
+		// 检查并调用事件，交换缓冲
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+
+	return 0;
+}
