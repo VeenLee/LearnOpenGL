3700a6467c00a6e70c445b47c47093f5e6b0b434
Lee <415049053@qq.com>
2020-03-08 21:47:42 +0800

    update

--- 2_04_LightingMaps/shader.fs
+++ 2_04_LightingMaps/shader.fs
@@ -3,8 +3,9 @@ out vec4 FragColor;
 
 struct Material {
     sampler2D diffuse;
-    vec3 specular;    
-    float shininess;
+    sampler2D specular;
+    sampler2D emission;
+	float shininess;
 }; 
 
 struct Light {
@@ -25,7 +26,7 @@ uniform Light light;
 
 void main()
 {
-    // ambient
+	// ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
@@ -38,8 +39,12 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
-        
-    vec3 result = ambient + diffuse + specular;
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+    
+    // emission
+    //vec3 emission = texture(material.emission, TexCoords).rgb;
+
+    //vec3 result = ambient + diffuse + specular + emission;
+	vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 } 
