86798e2cc29e2e02f67f1179bcb945d2f8c1a427
Lee <415049053@qq.com>
2020-06-11 18:20:36 +0800

    update

--- 6_01_PBR_Lighting/main.cpp
+++ 6_01_PBR_Lighting/main.cpp
@@ -31,7 +31,7 @@ bool hdrKeyPressed = false;
 float exposure = 1.0f;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 30.0f));
 float lastX = (float)SCR_WIDTH / 2.0;
 float lastY = (float)SCR_HEIGHT / 2.0;
 bool firstMouse = true;
