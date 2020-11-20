7ddbb09e734457bc39f9894f6288ff77ac62c75f
Lee <415049053@qq.com>
2020-11-20 10:05:12 +0800

    update

--- 5_05_NormalMapping/shader.fs
+++ 5_05_NormalMapping/shader.fs
@@ -1,6 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
+//TBN矩阵用法1：直接使用
+//in VS_OUT {
+//    vec3 FragPos;
+//    vec2 TexCoords;
+//    mat3 TBN;
+//} fs_in;
+
+//TBN矩阵用法2：使用逆矩阵
 in VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
@@ -26,12 +34,28 @@ void main()
     vec3 color = texture(diffuseMap, fs_in.TexCoords).rgb;
     // ambient
     vec3 ambient = 0.1 * color;
+
     // diffuse
+
+    //TBN矩阵用法1：直接使用
+    //normal = normalize(fs_in.TBN * normal);
+    //vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+
+    //TBN矩阵用法2：使用逆矩阵
     vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
+
+
+    //为何要用第二种方法呢？    
+    //将向量从世界空间转换到切线空间有个额外好处，我们可以把所有相关向量在顶点着色器中转换到切线空间，不用在像素着色器中做这件事。这是可行的，因为lightPos和viewPos不是每个fragment运行都要改变，对于fs_in.FragPos，我们也可以在顶点着色器计算它的切线空间位置。基本上，不需要把任何向量在像素着色器中进行变换，而第一种方法中就是必须的，因为采样出来的法线向量对于每个像素着色器都不一样。
+    //所以现在不是把TBN矩阵的逆矩阵发送给像素着色器，而是将切线空间的光源位置，观察位置以及顶点位置发送给像素着色器。这样我们就不用在像素着色器里进行矩阵乘法了。这是一个极佳的优化，因为顶点着色器通常比像素着色器运行的少。这也是为什么这种方法是一种更好的实现方式的原因。
+
+
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * color;
+
     // specular
-    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
+    //vec3 viewDir = normalize(viewPos - fs_in.FragPos); //TBN矩阵用法1：直接使用
+    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos); //TBN矩阵用法2：使用逆矩阵
     vec3 reflectDir = reflect(-lightDir, normal);
     vec3 halfwayDir = normalize(lightDir + viewDir);  
     float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
--- 5_05_NormalMapping/shader.vs
+++ 5_05_NormalMapping/shader.vs
@@ -5,6 +5,14 @@ layout (location = 2) in vec2 aTexCoords;
 layout (location = 3) in vec3 aTangent;
 layout (location = 4) in vec3 aBitangent;
 
+//TBN矩阵用法1：直接使用TBN矩阵，把切线空间的向量转换到世界空间
+//out VS_OUT {
+//    vec3 FragPos;
+//    vec2 TexCoords;
+//    mat3 TBN;
+//} vs_out;
+
+//TBN矩阵用法2：使用TBN矩阵的逆矩阵，把世界空间的向量转换到切线空间
 out VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
@@ -25,17 +33,26 @@ void main()
     vs_out.FragPos = vec3(model * vec4(aPos, 1.0));   
     vs_out.TexCoords = aTexCoords;
     
+    //将TBN向量变换到世界空间
     mat3 normalMatrix = transpose(inverse(mat3(model)));
     vec3 T = normalize(normalMatrix * aTangent);
     vec3 N = normalize(normalMatrix * aNormal);
+
+    //在大量的计算过程中，TBN向量可能会变得不两两垂直，这就会导致我们的模型有瑕疵。
+    //因此，一种名叫Gram-Schmidt正交化的方法就被创造出来。通过一点很小的代价，让TBN向量继续两两垂直：
     T = normalize(T - dot(T, N) * N);
+
+    //从技术上讲，顶点着色器中无需副切线B，TBN三向量都是相互垂直的所以可以用T和N向量的叉乘计算出副切线B
     vec3 B = cross(N, T);
-    
-    //创建一个TBN矩阵
-    mat3 TBN = transpose(mat3(T, B, N));    
+
+    //TBN矩阵用法1：直接使用TBN矩阵，把切线空间的向量转换到世界空间
+    //vs_out.TBN = mat3(T, B, N);
+
+    //TBN矩阵用法2：使用TBN矩阵的逆矩阵，把世界空间的向量转换到切线空间
+    mat3 TBN = transpose(mat3(T, B, N)); //正交矩阵（每个轴既是单位向量同时相互垂直）的一大属性是一个正交矩阵的转置矩阵与它的逆矩阵相等，这个属性很重要因为逆矩阵的求得比求转置开销大，但是结果却是一样的
     vs_out.TangentLightPos = TBN * lightPos;
     vs_out.TangentViewPos  = TBN * viewPos;
     vs_out.TangentFragPos  = TBN * vs_out.FragPos;
-        
+
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
