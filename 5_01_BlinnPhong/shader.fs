2fd61f58ed96996b3770e468a8778a0c91e83d85
VeenLee <415049053@qq.com>
2023-09-21 15:55:12 +0800

    Update shader.fs

--- 5_01_BlinnPhong/shader.fs
+++ 5_01_BlinnPhong/shader.fs
@@ -24,17 +24,16 @@ void main()
     vec3 diffuse = diff * color;
     // specular
     vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
     float spec = 0.0;
-    //Blinn-Phong�����ģ��Ψһ��������ǣ�Blinn-Phong�������Ƿ�����������֮��ļнǣ�������ģ�Ͳ������ǹ۲췽���뷴��������ļнǡ�
+    //Blinn-Phong与冯氏模型唯一的区别就是，Blinn-Phong测量的是法线与半程向量之间的夹角，而冯氏模型测量的是观察方向与反射向量间的夹角。
     if(blinn)
     {
-        //��ȡ�������
+        //获取半程向量
         vec3 halfwayDir = normalize(lightDir + viewDir);
-        //���㾵������:�Ա��淨�ߺͰ����������һ��Լ�����(Clamped Dot Product)���õ�˽����Ϊ�����Ӷ���ȡ����֮��нǵ�����ֵ��֮������ֵȡ����ȴη�
+        //计算镜面光分量:对表面法线和半程向量进行一次约束点乘(Clamped Dot Product)，让点乘结果不为负，从而获取它们之间夹角的余弦值，之后对这个值取反光度次方
         spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
 
-        //����ģ����Blinn-Phongģ��Ҳ��һЩϸ΢�Ĳ�𣺰����������淨�ߵļн�ͨ����С�ڹ۲��뷴�������ļнǡ����ԣ�������úͷ�����ɫ���Ƶ�Ч�����ͱ�����ʹ��Blinn-Phongģ��ʱ�����淴������ø���һ�㡣ͨ�����ǻ�ѡ�������ɫʱ����ȷ�����2��4����
+        //冯氏模型与Blinn-Phong模型也有一些细微的差别：半程向量与表面法线的夹角通常会小于观察与反射向量的夹角。所以，如果想获得和冯氏着色类似的效果，就必须在使用Blinn-Phong模型时将镜面反光度设置更高一点。通常我们会选择冯氏着色时反光度分量的2到4倍。
     }
     else
     {
